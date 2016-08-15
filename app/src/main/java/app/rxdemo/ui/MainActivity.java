af8ce10f6cf8371ba97fcfb5ee68937284791e21
Jorge Atempa <atempaitm@gmail.com>
2016-08-15 00:31:04 -0700

    Remover línea de importación

--- app/src/main/java/app/rxdemo/ui/MainActivity.java
+++ app/src/main/java/app/rxdemo/ui/MainActivity.java
@@ -1,7 +1,6 @@
 package app.rxdemo.ui;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
